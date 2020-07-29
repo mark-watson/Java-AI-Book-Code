062faf6c17b9d5f9e0f54ad5e1229ea240b0f9e4
Mark Watson <markw@markwatson.com>
2020-07-29 11:49:33 -0700

    removed build targets

--- kgn with menu/src/main/java/com/knowledgegraphnavigator/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.knowledgegraphnavigator;
-
-public class Utils {
-  static public String removeBrackets(String s) {
-    if (s.startsWith("<")) return s.substring(1, s.length() - 1);
-    return s;
-  }
-}
