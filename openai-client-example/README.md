d131baafce0abeaa0945d264cba0f7c54164f10b
Mark Watson <markw@markwatson.com>
2024-04-13 16:55:27 -0700

    starting OpenAI API client exmaple

---
+++ openai-client-example/README.md
@@ -0,0 +1,6 @@
+# OpenAI API Example
+
+**Under construction - don't use yet**
+
+Using library: https://github.com/TheoKanning/openai-java
+
---
+++ openai-client-example/src/main/java/com/markwatson/openai/App.java
@@ -0,0 +1,13 @@
+package com.markwatson.openai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
